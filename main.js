9b755b7038d5e42c17896101a9fa43fa35904718
Brian <brianjreynolds@hotmail.com>
2019-08-15 08:24:45 -0400

    First commit for DailyCodingProblem_8-14-19

---
+++ main.js
@@ -0,0 +1,12 @@
+/**
+ * Description: Given a list of numbers and a number k, return whether any two numbers 
+ * from the list add up to k.
+ * 
+ * Example: For the inputs arr = [10, 15, 3, 7] and k = 17, return true since 10 + 7 is 17.
+ * 
+ * Bonus: Can you do this in one pass?
+ */
+
+ function SolveProblem(arr, k) {
+ }
+ 
---
+++ test/main.Spec.js
@@ -0,0 +1,5 @@
+describe('SolveProblem([10, 15, 3, 7], 17)', function() {
+    it('should return 2', function() {
+        expect(SolveProblem([10, 15, 3, 7], 17)).toBe(true);
+    });
+});
