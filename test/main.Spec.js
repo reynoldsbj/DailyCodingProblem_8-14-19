a370b70b30794d90445324c36f75b50c7c0e9bfa
Brian <brianjreynolds@hotmail.com>
2019-08-15 19:24:08 -0400

    Added some more tests for DailyCodingProblem_8-14-19

--- test/main.Spec.js
+++ test/main.Spec.js
@@ -3,3 +3,21 @@ describe('SolveProblem([10, 15, 3, 7], 17)', function() {
         expect(SolveProblem([10, 15, 3, 7], 17)).toBe(true);
     });
 });
+
+describe('SolveProblem([1, 1, 1, 12, 1, 1, 1, 1, 1], 13)', function() {
+    it('should return true', function() {
+        expect(SolveProblem([1, 1, 1, 12, 1, 1, 1, 1, 1], 13)).toBe(true);
+    });
+});
+
+describe('SolveProblem([], 17)', function() {
+    it('should return false', function() {
+        expect(SolveProblem([], 17)).toBe(false);
+    });
+});
+
+describe('SolveProblem([10, 15, 3, 8], 17)', function() {
+    it('should return false', function() {
+        expect(SolveProblem([10, 15, 3, 8], 17)).toBe(false);
+    });
+});
